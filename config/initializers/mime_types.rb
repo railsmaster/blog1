f5c19704250e0b22d95d9065bc7c67baab2dfa3f
Ajay Raj Srivastava <ajayisready@gmail.com>
2017-06-13 15:15:14 +0530

    init

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
-Mime::Type.register "application/font-woff", :woff
-Mime::Type.register "application/font-woff", :woff2
+
+Rack::Mime::MIME_TYPES['.woff'] = 'application/x-font-woff'
+Rack::Mime::MIME_TYPES['.woff'] = 'application/x-font-woff2'
